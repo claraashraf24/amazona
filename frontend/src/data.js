f2977d0359cc4dd2fe1247363dd549141d2a6a98
CLARA_YOUSSEF <claraashraf2411@gmail.com>
2023-06-15 15:25:45 +0300

    home-screen-and-product-screen

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'The Wallows band shirt',
+      slug: 'the-wallows-band-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', //679px x 829px
+      price: 50,
+      countInStock: 20,
+      brand: 'Unknown',
+      rating: 3.8,
+      numReviews: 7,
+      description: 'high quality merch',
+    },
+    {
+      name: 'Stranger Things shirt',
+      slug: 'stranger-things-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg', //679px x 829px
+      price: 80,
+      countInStock: 20,
+      brand: 'Unknown',
+      rating: 4.7,
+      numReviews: 20,
+      description: 'very high quality merch',
+    },
+    {
+      name: 'Sarsag Pants',
+      slug: 'sarsag-pants',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 20,
+      countInStock: 40,
+      brand: 'Wekala',
+      rating: 1.2,
+      numReviews: 50,
+      description: 'poor quality pants',
+    },
+  ],
+};
+
+export default data;
